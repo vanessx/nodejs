7038fd227068a85567028b37291042ba4ec918b9
vanessa <51798590+vanessx@users.noreply.github.com>
2024-12-17 15:13:56 +0100

    commit

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -1,15 +1,26 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+const readFilePromise = (file) => {
+	return new Promise((resolve, reject) => {
+		fs.readFile(file, (err, data) => {
+			if (err) reject("I could not find that file");
+			resolve(data);
+		});
+	});
+};
+
+readFilePromise(`${__dirname}/dog.txt`).then((data) => {
 	superagent
 		.get(`https://dog.ceo/api/breed/${data}/images/random`)
-		.end((err, res) => {
-			if (err) return console.log(err.message);
+		.then((res) => {
 			console.log(res.body.message);
 
 			fs.writeFile("dog-img.txt", res.body.message, (err) => {
 				console.log("Random dog image saved to file.");
 			});
+		})
+		.catch((err) => {
+			console.log(err.message);
 		});
 });
